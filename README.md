4ded18acd1a3e342d5bebef991a1ee6677b22ca7
Serge Mymrikov <62348583+PhantomCat@users.noreply.github.com>
2024-04-30 16:12:57 +0300

    README update, some new files

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Useful linux scripts
 
-## Samba
-### AD and domain membership
-- If you need to clean all samba database files (if you're experimenting with AD membership in samba)[clean_all_bd script](samba/ad/clean_all_bd.sh)
+### Samba
+#### AD and domain membership
+- If you need to clean all samba database files (if you're experimenting with AD membership in samba)[clean_all_bd script](linux/samba/ad/clean_all_bd.sh)
 - to add your Samba server to AD first - save your own smb.conf, nsswitch.conf, krb5.conf
 
 ```bash
@@ -13,11 +13,12 @@ sudo mv /etc/samba/smb.conf ./smb_backup/smb.conf.$(date +"%Y-%m%-%d").bak
 sudo mv /etc/nsswitch.conf ./smb_backup/nsswitch.conf.$(date +"%Y-%m%-%d").bak
 sudo mv /etc/krb5.conf ./smb_backup/krb5.conf.$(date +"%Y-%m%-%d").bak
 ```
-use [this smb.conf](samba/ad/smb.conf) as a sample.
+use [this smb.conf](linux/samba/ad/smb.conf) as a sample.
 
-## bash
+### bash
 
-To make your bash looks awesome you don't need to use zsh with 1001 stories (I know, it's powerfull and handy, but if the point only in good-looking then why?)
+#### awesome bash input string (zsh+PowerLevel10K-like)
+To make your bash looks awesome you don't need to use zsh with 10K stories (I know, it's powerfull and handy, but if the point only in good-looking then why?)
 open your .bashrc file with your favorite editor (I bet - it's vim!) and change the PS1 string to look like this:
 ```bash
 PS1='${debian_chroot:+($debian_chroot)}\[\033[42m\033[02;30m\]\u\033[43m@\h\[\033[00m\]\[\033[01;44m\]:\w\[\033[00m\]$(__git_ps1)\n└─ $ '
@@ -31,3 +32,10 @@ It will be represented like this:
 ![shining_bash.png](screenshots/shining_bash.png)
 
 yeah, it also shows a git branch if the directory is a git repository
+
+## Windows
+
+### Backup rotation for MS SQL
+
+- this [script](windows/backup_rotation.bat) is running through directories and copy .bak files in directories of base, year and month. Backups should be on D: in Backups subfolders. .bak files in Backups directory will not be copied (my developers use them as temporary backups)
+
---
+++ windows/backup_rotation.bat
@@ -0,0 +1,88 @@
+@echo off
+REM ротация резервных копий MS SQL (.bak)
+
+D:
+CD D:\Backups
+
+if NOT EXIST S: net use S: \\your.serv\backup
+
+set BDIR=S:\SQL_Backup
+set YEAR=%DATE:~6%
+set MONTH=%DATE:~3,2%
+set DAY=%DATE:~0,2%
+set BDAY=Sunday
+set PREFIX=daily
+REM Присвоить переменной текущего дня значение дня
+for /f %%i in ('powershell ^(get-date^).DayOfWeek') do set DOW=%%i
+set LOGFILE=%BDIR%\_LOGS\log_%YEAR%-%MONTH%-%DAY%.log
+echo ------------------------------- >> %LOGFILE%
+
+if %DAY% == 01 ( 
+		REM Если первое число месяца - перемещаем в папку с номером месяца и префиксом monthly_
+		set PREFIX=monthly
+		set EXPIRE=183
+		echo Сегодня 1 число, нужно сделать ежемесячную резервную копию >> %LOGFILE%
+	) else if %DOW%==%BDAY% (
+		REM Если Воскресенье, то переместить бекап с именем weekly_
+		set PREFIX=weekly
+		set EXPIRE=16
+		echo Сегодня %BDAY%, нужно сделать еженедельную резервную копию >> %LOGFILE%
+	) else (
+		REM Производим ежедневную работу ротации
+		set PREFIX=daily
+		set EXPIRE=7
+		echo Перемещаем .bak в папку  текущего месяца >> %LOGFILE%
+		echo ------------------------------- >> %LOGFILE%
+	)
+	
+echo ---------------------------- %PREFIX% >> %LOGFILE%
+echo ---------------------------- %EXPIRE% >> %LOGFILE%
+
+call :filesProcess
+copy %LOGFILE% D:\Backups\_LOGS\
+net use /d S:
+goto :eof
+
+:filesProcess
+REM если мы НЕ в корневой папке бекапов - делаем бекапы
+if %cd% NEQ D:\Backups (
+	echo ОТЛАДКА: %basedir% >> %LOGFILE%
+
+	
+	for %%z in (*.bak) do (
+		if not exist %BDIR%\%basedir%\%YEAR%\%MONTH%\monthly_%%z (
+			if not exist %BDIR%\%basedir%\%YEAR%\%MONTH%\weekly_%%z (
+				if not exist %BDIR%\%basedir%\%YEAR%\%MONTH%\daily_%%z (
+					certutil -hashfile %%z MD5 >> %BDIR%\%basedir%\%YEAR%\%MONTH%\%PREFIX%_%%z.md5
+					copy "%%z" "%BDIR%\%basedir%\%YEAR%\%MONTH%\%PREFIX%_%%z" >> %LOGFILE%
+				)
+			)
+		) else echo Файл %BDIR%\%basedir%\%YEAR%\%MONTH%\monthly_%%z уже имеет резервную копию, пропускаю >> %LOGFILE%
+			
+	)
+	echo forfiles -p "%BDIR%\%basedir%" -s -m %PREFIX%_*.* -d -%EXPIRE% -c "cmd /c del /q @path" >> %LOGFILE%
+
+	:delstep
+	echo Удаляем .trn и .bak файлы старше одного дня в папке %basedir% >> %LOGFILE%
+	forfiles /P "." /M *.trn /D -1 /C "cmd /c del /q @file" >> %LOGFILE%
+	forfiles /P "." /M *.bak -d -1 /C "cmd /c del /q @path" >> %LOGFILE%
+	
+	REM echo \n  >> %LOGFILE%
+	echo ------------------------------- >> %LOGFILE%
+	echo Закончили с базой %basedir% >> %LOGFILE%
+	echo ------------------------------- >> %LOGFILE%
+	REM echo \n  >> %LOGFILE%
+	)
+)
+
+for /D %%d in (*) do (
+	REM Если нет папки года-месяца - нужно их создать
+	if NOT EXIST "%BDIR%\%%d\%YEAR%\%MONTH%" mkdir "%BDIR%\%%d\%YEAR%\%MONTH%"
+	echo I'm in %%d  >> %LOGFILE%
+	cd %%d
+	set basedir=%%d
+	call :filesProcess
+	cd ..
+)
+
+
