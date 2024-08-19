83b513a097cd03780eaabc3c0fbb82e97eff2e7f
Serge Mymrikov <62348583+PhantomCat@users.noreply.github.com>
2024-08-19 10:37:54 +0300

    Update docker_builder_cache_cleaner.sh

--- linux/docker/docker_builder_cache_cleaner.sh
+++ linux/docker/docker_builder_cache_cleaner.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-left=$(df -h | awk '/\/$/ {print $--NF-0}')
-if [ $left -le 10 ]
+used=$(df -h | awk '/\/$/ {print $--NF-0}')
+if [ $used -ge 90 ]
 then 
-	echo "$(date) - DOCKER BUILDER CACHE CLEANER - Less, than 10% left on / ($left%), pruning docker builder cache" >> /var/log/messages
-	docker builder prune && 
+	echo "$(date) - DOCKER BUILDER CACHE CLEANER - Less, than 10% left on / ($(echo 100 - $used | bc)%), pruning docker builder cache" >> /var/log/messages
+	docker builder prune -f --filter until=48h && 
 		echo "$(date) - DOCKER BUILDER CACHE CLEANER - Done"  >> /var/log/messages || 
 		echo "$(date) - DOCKER BUILDER CACHE CLEANER - ERROR!"  >> /var/log/messages
 else
 	echo "$(date) - DOCKER BUILDER CACHE CLEANER - More, than 10% left on / ($left%), unneed to prune docker builder cache" >> /var/log/messages
 fi
-
-
